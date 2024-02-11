a4a30a2304a669687104e40816007afdfb83e3cf
alieissa <aeissa.o@gmail.com>
2024-02-11 16:52:36 -0500

    #29 Define routes for each module under separate scopes.

--- lib/spaced_rep_web/router.ex
+++ lib/spaced_rep_web/router.ex
@@ -5,15 +5,32 @@ defmodule SpacedRepWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", SpacedRepWeb do
+  scope "/decks/:deck_id/cards/:card_id/answers", SpacedRepWeb do
+    pipe_through :api
+    get "/", AnswerController, :index
+    post "/", AnswerController, :create
+    get "/:id", AnswerController, :show
+    put "/:id", AnswerController, :update
+    delete "/:id", AnswerController, :delete
+    post "/check", AnswerController, :check
+  end
+
+  scope "/decks/:deck_id/cards", SpacedRepWeb do
+    pipe_through :api
+    get "/", CardController, :index
+    post "/", CardController, :create
+    get "/:id", CardController, :show
+    put "/:id", CardController, :update
+    delete "/:id", CardController, :delete
+  end
+
+  scope "/decks", SpacedRepWeb do
     pipe_through :api
 
-    post "decks/:deck_id/cards/:card_id/answers/check", AnswerController, :check
-    # TODO Unnest routes
-    resources "decks", DeckController, except: [:new, :edit] do
-      resources "cards", CardController, except: [:new, :edit] do
-        resources "answers", AnswerController, except: [:new, :edit]
-      end
-    end
+    get "/", DeckController, :index
+    post "/", DeckController, :create
+    get "/:id", DeckController, :show
+    put "/:id", DeckController, :update
+    delete "/:id", DeckController, :delete
   end
 end
