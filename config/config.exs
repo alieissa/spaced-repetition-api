6145be51aa6ba5650683091dcaf35c7f20354fc5
alieissa <aeissa.o@gmail.com>
2024-05-02 16:42:00 -0400

    39 Rename keys so that they are obviously for S3.

--- config/config.exs
+++ config/config.exs
@@ -30,8 +30,8 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 config :ex_aws,
-  access_key_id: {:system, "AWS_ACCESS_KEY"},
-  secret_access_key: {:system, "AWS_SECRET_ACCESS_KEY"},
+  access_key_id: {:system, "AWS_S3_ACCESS_KEY"},
+  secret_access_key: {:system, "AWS_S3_SECRET_ACCESS_KEY"},
   region: {:system, "AWS_REGION"}
 
 # Import environment specific config. This must remain at the bottom
