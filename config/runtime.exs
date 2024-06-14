c65b624ed2e839a9613286ceda3696444eada8ce
ali <aeissa.o@gmail.com>
2024-06-14 11:38:34 -0400

    46 download endpoint broken (#50)
    * 46 Clean up the configs a bit.
    
    * 46 Use proper decks list function.

--- config/runtime.exs
+++ config/runtime.exs
@@ -20,21 +20,16 @@ if System.get_env("PHX_SERVER") do
   config :spaced_rep, SpacedRepWeb.Endpoint, server: true
 end
 
-if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
-
+if config_env() == :prod || config_env() == :dev do
   config :spaced_rep, SpacedRep.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    hostname:
+      System.get_env("DB_HOSTNAME") || raise("Environment variable DB_HOSTNAME is not set."),
+    database: System.get_env("DB_NAME") || raise("Environment variable DB_NAME is not set."),
+    username:
+      System.get_env("DB_USERNAME") || raise("Environment variable DB_USERNAME is not set."),
+    password:
+      System.get_env("DB_PASSWORD") || raise("Environment variable DB_PASSWORD is not set."),
+    pool_size: 10
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -48,14 +43,21 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
   config :spaced_rep, SpacedRepWeb.Endpoint,
     http: [
       ip: {0, 0, 0, 0},
-      port: port
+      port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
+
+  config :ex_aws,
+    access_key_id:
+      System.get_env("AWS_S3_ACCESS_KEY") ||
+        raise("Environment variable AWS_S3_ACCESS_KEY is not set."),
+    secret_access_key:
+      System.get_env("AWS_S3_SECRET_ACCESS_KEY") ||
+        raise("Environment variable AWS_S3_SECRET_ACCESS_KEY is not set."),
+    region: System.get_env("REGION") || raise("Environment variable REGION is not set.")
 
   # ## SSL Support
   #
