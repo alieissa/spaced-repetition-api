24c6743728280c26655a039ce4aaf5f3e7aa9917
alieissa <aeissa.o@gmail.com>
2024-06-18 17:44:41 -0400

    49 Add user id to conn.

---
+++ lib/spaced_rep_web/plugs/user_id.ex
@@ -0,0 +1,21 @@
+# Reference: https://blog.jola.dev/health-checks-for-plug-and-phoenix
+defmodule SpacedRepWeb.Plugs.UserID do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(%Plug.Conn{request_path: "/health"} = conn, _opts) do
+    conn
+    |> send_resp(200, "healthy")
+    |> halt()
+  end
+
+  def call(conn, _opts) do
+    with [token] <- get_req_header(conn, "authorization"),
+         %JOSE.JWT{fields: %{"sub" => user_id}} <- JOSE.JWT.peek_payload(token) do
+      assign(conn, :user_id, user_id)
+    else
+      _ -> conn |> send_resp(404, "Nothing found") |> halt()
+    end
+  end
+end
